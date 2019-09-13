2fa1d35fafbf5963fdd53f76ec1c31c13f062c36
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-09-13 17:39:55 +0800

    Implement A* Tree Search with Manhattan Distance

--- programs/puzzle_A1_43_1.py
+++ programs/puzzle_A1_43_1.py
@@ -1,4 +1,9 @@
-import os
+"""
+A* search using Manhattan distance heuristic.
+"""
+
+import copy
+import heapq
 import sys
 
 
@@ -7,36 +12,105 @@ class Puzzle(object):
         # You may add more attributes as necessary
         self.init_state = init_state
         self.goal_state = goal_state
-        self.actions = list()
-        self.solvable = True
 
         self.size = 3
 
     def h(self, state):
-        """Returns sum of manhattan distances of all tiles except blank tile."""
-        cost = 0
-        for row in range(self.size):
-            for col in range(self.size):
-                if state[row][col] != 0:
-                    goal_r = (state[row][col] - 1) // self.size;
-                    goal_c = (state[row][col] - 1) % self.size;
-                    cost += abs(goal_r - row) + abs(goal_c - col)
-        return cost
+        """
+        Returns sum of manhattan distances of all tiles except blank tile.
+        """
+        loc = {
+            val: (i, j)
+            for i, row in enumerate(self.goal_state)
+            for j, val in enumerate(row)
+        }
 
-    def eval_score(self, step, state):
-        """Returns evaluation function f(x) = g(x) + h(x)."""
-        return step + self.h(state)
+        def calculate_cost(i, j):
+            val = state[i][j]
+            if val == 0:
+                return 0
+            goal_i, goal_j = loc[val]
+            return abs(goal_i - i) + abs(goal_j - j)
+
+        return sum(
+            calculate_cost(i, j) for i in range(self.size)
+            for j in range(self.size))
 
     def solve(self):
-        # TODO: Write your code here
-        # return: a list of actions like: ["UP", "DOWN"]
-        print(self.init_state, self.goal_state)
-        print(self.heuristic(self.init_state))
-        return []
+        def find_blank(state):
+            for i in range(self.size):
+                for j in range(self.size):
+                    if state[i][j] == 0:
+                        return (i, j)
+
+        def generate_state(old_state, action, blank):
+            state = copy.deepcopy(old_state)
+            i, j = blank
+            if action == "UP":
+                state[i][j], state[i + 1][j] = state[i + 1][j], state[i][j]
+            if action == "DOWN":
+                state[i][j], state[i - 1][j] = state[i - 1][j], state[i][j]
+            if action == "LEFT":
+                state[i][j], state[i][j + 1] = state[i][j + 1], state[i][j]
+            if action == "RIGHT":
+                state[i][j], state[i][j - 1] = state[i][j - 1], state[i][j]
+            return state
+
+        def successors(state):
+            actions = []
+            blank_i, blank_j = blank = find_blank(state)
+            if blank_i < self.size - 1:
+                actions.append("UP")
+            if blank_i > 0:
+                actions.append("DOWN")
+            if blank_j < self.size - 1:
+                actions.append("LEFT")
+            if blank_j > 0:
+                actions.append("RIGHT")
+            return [(action, generate_state(state, action, blank))
+                    for action in actions]
+
+        pred = {}
+
+        def backtrack(state):
+            result = []
+            cur = str(state)
+            str_init = str(self.init_state)
+            while cur != str_init:
+                cur, action = pred[cur]
+                result.append(action)
+            return result[::-1]
+
+        # Frontier is (f, state, g)
+        frontier = [(self.h(self.init_state), self.init_state, 0)]
+        frontier_set = set([str(self.init_state)])
+        visited = set()
+        while frontier:
+            _, state, g = heapq.heappop(frontier)
+            frontier_set.remove(str(state))
+
+            # The state was in the frontier
+            # (cheaper to check this way than when adding)
+            if str(state) in visited:
+                continue
+
+            if state == self.goal_state:
+                return backtrack(state)
+            visited.add(str(state))
+            new_g = g + 1
+            for new_action, new_state in successors(state):
+                str_state = str(new_state)
+                if str_state not in visited and str_state not in frontier_set:
+                    pred[str_state] = (str(state), new_action)
+                    new_score = new_g + self.h(new_state)
+                    frontier_set.add(str_state)
+                    heapq.heappush(frontier, (new_score, new_state, new_g))
+        return ["UNSOLVABLE"]
 
     # You may add more (helper) methods if necessary.
     # Note that our evaluation scripts only call the solve method.
-    # Any other methods that you write should be used within the solve() method.
+    # Any other methods that you write should be used within the solve() method
+
 
 if __name__ == "__main__":
     # do NOT modify below
@@ -52,10 +126,10 @@ if __name__ == "__main__":
     goal_state = [[0 for i in range(3)] for j in range(3)]
     lines = f.readlines()
 
-    i,j = 0, 0
+    i, j = 0, 0
     for line in lines:
         for number in line:
-            if '0'<= number <= '8':
+            if '0' <= number <= '8':
                 init_state[i][j] = int(number)
                 j += 1
                 if j == 3:
@@ -63,7 +137,7 @@ if __name__ == "__main__":
                     j = 0
 
     for i in range(1, 9):
-        goal_state[(i-1)//3][(i-1)%3] = i
+        goal_state[(i - 1) // 3][(i - 1) % 3] = i
     goal_state[2][2] = 0
 
     puzzle = Puzzle(init_state, goal_state)
@@ -71,11 +145,4 @@ if __name__ == "__main__":
 
     with open(sys.argv[2], 'a') as f:
         for answer in ans:
-            f.write(answer+'\n')
-
-
-
-
-
-
-
+            f.write(answer + '\n')
